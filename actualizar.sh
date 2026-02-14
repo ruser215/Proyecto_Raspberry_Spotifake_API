e16bc2264704bb592b183da650d272ae3754ad44
ruser215 <www.ruser215@gmail.com>
2026-02-14 21:36:13 +0100

    Me voy a duchar mientras esto intenta hacer algo

--- actualizar.sh
+++ actualizar.sh
@@ -1,69 +1,34 @@
 #!/usr/bin/env bash
 
-
-
 set -e  # Detener si algo falla
 
-
-
 echo "========================================="
-
 echo " Iniciando actualización de la API"
-
 echo "========================================="
-
-
 
 # --- Comprobar que estamos en repo git ---
 
-
-
 if [ ! -d ".git" ]; then
-
 echo "Este directorio no es un repositorio Git"
-
 exit 1
-
 fi
-
-
 
 # --- Actualizar código ---
 
-
-
 echo "Descargando cambios de GitHub..."
-
 git fetch origin
-
 git pull --rebase origin main
 
+# --- Actualizar contenedor API ---
 
-
-# --- Reconstruir contenedor API ---
-
-
-
-
-echo "Reconstruyendo contenedor Ktor..."
-
-
-docker compose up -d --build ktor-app
-
-
+echo "Actualizando contenedor Ktor..."
+docker compose up -d --build --force-recreate ktor-app
 
 # --- Limpiar imágenes antiguas ---
 
-
-
 echo "Limpiando imágenes colgantes..."
-
 docker image prune -f
 
-
-
 echo "Actualización completada"
-
 echo "👉 Base de datos y servicios auxiliares intactos"
-
 echo "========================================="
