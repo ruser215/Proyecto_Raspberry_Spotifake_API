161acfaed7d7627607051347a626e1a32519ff37
ruser215 <www.ruser215@gmail.com>
2026-04-01 12:23:06 +0200

    Prueba a eliminar la persistencia

--- src/main/kotlin/com/data/persistence/SuspendTransaction.kt
+++
@@ -1,7 +0,0 @@
-package com.data.persistence
-
-import kotlinx.coroutines.Dispatchers
-import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
-
-suspend fun <T> suspendTransaction(block: suspend () -> T): T =
-    newSuspendedTransaction(Dispatchers.IO) { block() }
