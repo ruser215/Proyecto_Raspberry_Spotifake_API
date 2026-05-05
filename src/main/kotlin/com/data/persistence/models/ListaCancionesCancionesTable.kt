4f3c26a9e3ce7c0cc4155cad9a0e964b0fff12c9
ruser215 <www.ruser215@gmail.com>
2026-05-05 12:38:30 +0200

    feat: añadir onDelete CASCADE a vinculaciones de las listas de canciones

--- src/main/kotlin/com/data/persistence/models/ListaCancionesCancionesTable.kt
+++ src/main/kotlin/com/data/persistence/models/ListaCancionesCancionesTable.kt
@@ -1,9 +1,10 @@
 package com.data.persistence.models
 
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
 object ListaCancionesCancionesTable : Table("lista_canciones_canciones") {
-    val idCancion = reference("id_cancion", CancionTable)
-    val idLista = reference("id_lista", ListaCancionesTable)
+    val idCancion = reference("id_cancion", CancionTable, onDelete = ReferenceOption.CASCADE)
+    val idLista = reference("id_lista", ListaCancionesTable, onDelete = ReferenceOption.CASCADE)
     override val primaryKey = PrimaryKey(idCancion, idLista)
 }
--- src/main/kotlin/com/data/persistence/models/ListaCancionesTable.kt
+++ src/main/kotlin/com/data/persistence/models/ListaCancionesTable.kt
@@ -1,8 +1,9 @@
 package com.data.persistence.models
 
 import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.ReferenceOption
 
 object ListaCancionesTable : LongIdTable("lista_canciones") {
     val nombre = varchar("nombre", 100)
-    val idUsuario = reference("id_usuario", UsuarioTable)
+    val idUsuario = reference("id_usuario", UsuarioTable, onDelete = ReferenceOption.CASCADE)
 }
